eb28e3634225f0e1e7f3943d9e1c406fb6f5ee61
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-07-19 18:04:20 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,30 @@
+// console.log("Hello World...");
+// console.log('Welcome to Angular Module');
+// console.log(`
+//             Ur First typescript Pgm is Execute...
+//     `);
+var a1;
+var a_1;
+// data type 
+// number(int,long,double,float,short),string(''/""),boolean(true/flase)
+// any (number,string,boolean)
+// void (netural data type => it does not point any data type)
+// misec null & undefined => it works as data type as well as value 
+var a = 10; // forward declaration & defination
+//  console.log("Value of a is "+a);
+//  console.log(`
+//         value of A is ${a}
+//     `);
+var str;
+// console.log("value of str is "+str)
+// str='Sumit';
+// console.log("value of str is "+str);
+var c;
+c = 2.5;
+console.log("Value of c is " + c);
+c = 'Sumit';
+console.log("Value of c is " + c);
+c = true;
+console.log("Value of c is " + c);
+var d = 56;
+console.log("value of D is " + d);
