adb81efa36482dcbcc201cd139a987a3ced75444
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-08-14 18:06:51 +0530

    Day06

---
+++ Angular/Day06/ifdirective/ifdirective.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ifdirective',
+  templateUrl: './ifdirective.component.html',
+  styleUrls: ['./ifdirective.component.css']
+})
+export class IfdirectiveComponent {
+  rating:number=2;
+}
