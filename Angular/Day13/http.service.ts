dd36677771b7e1fa43accb04724456866700e1c9
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-08-30 18:14:23 +0530

    Day13

---
+++ Angular/Day13/http.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+private url:string="https://jsonplaceholder.typicode.com/posts";
+  constructor(private http:HttpClient) { }
+
+  getPost(){
+    return (this.http.get(this.url));
+  }
+
+  postData(obj:any){
+   return (this.http.post(this.url,obj));
+  }
+
+}
---
+++ Angular/Day13/post/post.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts:any[]=[];
+
+  constructor(private service:HttpService){}
+  ngOnInit(): void {
+    this.getDataFromBackend();
+  }
+
+  getDataFromBackend(){
+    this.service.getPost()
+    .subscribe((response:any)=>{
+      // console.log(response);
+      this.posts=response;
+
+    })
+  }
+
+  onSend(mytitle:string,mybody:string){
+      let jsonObj={
+          title:mytitle,
+          body:mybody
+      }
+      this.service.postData(jsonObj)
+      .subscribe((response)=>{
+        console.log(response);
+      })
+  }
+
+}
