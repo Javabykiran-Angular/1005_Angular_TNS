fcc5512b803e840d7a31829626630a85293cbacd
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-23 17:29:41 +0530

    Login

---
+++ Angular/Day23/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+
+  incorrect:string='';
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+  
+  ngOnInit(): void {
+   document.body.className='bg_background';
+  }
+
+
+  onSubmit(f:NgForm){
+
+  }
+
+}
