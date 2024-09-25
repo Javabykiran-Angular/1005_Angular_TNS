46d2f4add2b998ced93f21a62d6f32b78a7ac46c
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-25 17:53:51 +0530

    Remaining Files

---
+++ Angular/Day24/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const routes: Routes = [
+  {
+    path:"",pathMatch:"full",redirectTo:"/login"
+  },
+  {
+    path:"login",component:LoginComponent
+  },
+  {
+    path:"home",component:HomeComponent
+  },
+  {
+    path:"**",component:NotfoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
